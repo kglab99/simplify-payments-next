4258981c06f61a1cfc532897acd2bb446ffa6910
kglab99 <kglab.photo@gmail.com>
2024-07-28 11:24:16 +0200

    groups list using accordion to keep styling between pages consistent

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,50 +1,79 @@
 import {
   Navbar,
   NavbarBrand,
-  NavbarContent,
-  NavbarItem,
+  Breadcrumbs,
+  BreadcrumbItem,
 } from "@nextui-org/react";
-import { Button, ButtonGroup } from "@nextui-org/button";
+import { Button } from "@nextui-org/button";
+import { Listbox, ListboxItem } from "@nextui-org/listbox";
 import AddIcon from "@mui/icons-material/Add";
-import { Listbox, ListboxSection, ListboxItem } from "@nextui-org/listbox";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import { Link } from "react-router-dom";
-import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Tabs, Tab } from "@nextui-org/tabs";
+import { Accordion, AccordionItem } from "@nextui-org/accordion";
 
 export default function HomePage({ groups }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col ">
-      <Navbar maxWidth="full" isBordered={true} isBlurred={true}>
+    <div className="flex flex-col">
+      <Navbar maxWidth="full" isBordered isBlurred>
         <NavbarBrand>
           <p className="font-bold text-inherit">Simplify Payments</p>
         </NavbarBrand>
       </Navbar>
-      <div className="px-6 py-4">
+
+      <div className="flex flex-col gap-4 px-6 py-4">
         <Breadcrumbs>
           <BreadcrumbItem>Groups</BreadcrumbItem>
         </Breadcrumbs>
 
-        <Listbox>
-          {groups.map((group) => (
-            <ListboxItem key={group.id} endContent={<ChevronRightIcon />} onClick={() => navigate(`/group/${group.id}`)}>
-              <h2 className="text-xl font-bold">{group.groupName}</h2>
-              <p> {Object.values(group.users).join(", ")}</p>
-            </ListboxItem>
-          ))}
-        </Listbox>
+        <div className="flex flex-col">
+          <Tabs aria-label="Options" variant="solid" radius="sm" fullWidth>
+            <Tab title="Groups">
+              {groups.length > 0 ? (
+                <Listbox>
+                  {groups.map((group) => (
+                    <ListboxItem
+                      key={group.id}
+                      showDivider
+                      // endContent={<ChevronRightIcon />}
+                    >
+                      <Accordion>
+                        <AccordionItem
+                          className="custom-accordion-item"
+                          title={group.groupName}
+                          subtitle={Object.values(group.users).join(", ")}
+                          // hideIndicator
+                          // disableAnimation
+                          onPress={() => navigate(`/group/${group.id}`)}
+                        >
+                       
+                        </AccordionItem>
+                      </Accordion>
+                    </ListboxItem>
+                  ))}
+                </Listbox>
+              ) : (
+                <div className="py-4">
+                  <p>No groups found. Create a new group to get started!</p>
+                </div>
+              )}
+            </Tab>
+
+            <Tab title="Settings"></Tab>
+          </Tabs>
+        </div>
       </div>
 
       <Link className="self-center" to="/create-group">
         <Button
           variant="flat"
           size="md"
-          //   color="primary"
           radius="sm"
           startContent={<AddIcon />}
           className="text-black"
+          aria-label="Create new group"
         >
           Create new group
         </Button>
